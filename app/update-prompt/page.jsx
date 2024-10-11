d0191f72e790d229942bfec97e1b845310afe969
RenzPaoloAyuban <renzpaoloayuban01>
2024-10-11 15:23:33 +0800

    make cancel button in forms dynamic

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -60,9 +60,13 @@ const UpdatePrompt = () => {
             submitting={submitting}
             handleSubmit={updatePrompt}
 <<<<<<< HEAD
+<<<<<<< HEAD
             isUpdateMode={true}
 =======
 >>>>>>> 1edbc47 (almost finished, search bar completed)
+=======
+            isUpdateMode={true}
+>>>>>>> 589f005 (make cancel button in forms dynamic)
         />
     );
 };
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,13 +1,18 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 "use client";
 =======
 'use client';
 >>>>>>> 1edbc47 (almost finished, search bar completed)
+=======
+"use client";
+>>>>>>> 589f005 (make cancel button in forms dynamic)
 
 import { useState } from "react";
 import Image from "next/image";
 import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
+<<<<<<< HEAD
 <<<<<<< HEAD
 
 const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
@@ -18,11 +23,18 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
     const [copied, setCopied] = useState("");
 
 >>>>>>> 1edbc47 (almost finished, search bar completed)
+=======
+
+const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
+>>>>>>> 589f005 (make cancel button in forms dynamic)
     const { data: session } = useSession();
     const pathName = usePathname();
     const router = useRouter();
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 589f005 (make cancel button in forms dynamic)
     const [copied, setCopied] = useState("");
 
     const handleProfileClick = () => {
@@ -32,12 +44,16 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 
         router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
     };
+<<<<<<< HEAD
 =======
 >>>>>>> 1edbc47 (almost finished, search bar completed)
+=======
+>>>>>>> 589f005 (make cancel button in forms dynamic)
 
     const handleCopy = () => {
         setCopied(post.prompt);
         navigator.clipboard.writeText(post.prompt);
+<<<<<<< HEAD
 <<<<<<< HEAD
         setTimeout(() => setCopied(false), 3000);
     };
@@ -65,24 +81,42 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 =======
         setTimeout(() => setCopied(""), 3000);
     }
+=======
+        setTimeout(() => setCopied(false), 3000);
+    };
+>>>>>>> 589f005 (make cancel button in forms dynamic)
 
     return (
-        <div className="prompt_card">
-            <div className="flex justify-between items-start gap-5">
-                <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
-                    <Image src={post.creator.image} alt="user_image" width={40} height={40} className="rounded-full object-contain"></Image>
+        <div className='prompt_card'>
+            <div className='flex justify-between items-start gap-5'>
+                <div
+                    className='flex-1 flex justify-start items-center gap-3 cursor-pointer'
+                    onClick={handleProfileClick}
+                >
+                    <Image
+                        src={post.creator.image}
+                        alt='user_image'
+                        width={40}
+                        height={40}
+                        className='rounded-full object-contain'
+                    />
 
-                    <div className="dlex flex-col">
-                        <h3 className="font-satoshi font-semibold text-gray-700">
+                    <div className='flex flex-col'>
+                        <h3 className='font-satoshi font-semibold text-gray-900'>
                             {post.creator.username}
                         </h3>
+<<<<<<< HEAD
                         <p className="font-inter text-sm text-gray-500">
 >>>>>>> 1edbc47 (almost finished, search bar completed)
+=======
+                        <p className='font-inter text-sm text-gray-500'>
+>>>>>>> 589f005 (make cancel button in forms dynamic)
                             {post.creator.email}
                         </p>
                     </div>
                 </div>
 
+<<<<<<< HEAD
 <<<<<<< HEAD
                 <div className='copy_btn' onClick={handleCopy}>
                     <Image
@@ -129,34 +163,53 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 export default PromptCard;
 =======
                 <div className="copy_btn" onClick={handleCopy}>
+=======
+                <div className='copy_btn' onClick={handleCopy}>
+>>>>>>> 589f005 (make cancel button in forms dynamic)
                     <Image
-                        src={copied === post.prompt
-                            ? '/assets/icons/tick.svg'
-                            : 'assets/icons/copy.svg'}
+                        src={
+                            copied === post.prompt
+                                ? "/assets/icons/tick.svg"
+                                : "/assets/icons/copy.svg"
+                        }
+                        alt={copied === post.prompt ? "tick_icon" : "copy_icon"}
                         width={12}
-                        height={12} />
+                        height={12}
+                    />
                 </div>
             </div>
 
-            <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
-            <p className="font-inter text-sm blue_gradient cursor-pointer"
-                onClick={() => handleTagClick && handleTagClick(post.tag)}>{post.tag}
+            <p className='my-4 font-satoshi text-sm text-gray-700'>{post.prompt}</p>
+            <p
+                className='font-inter text-sm blue_gradient cursor-pointer'
+                onClick={() => handleTagClick && handleTagClick(post.tag)}
+            >
+                #{post.tag}
             </p>
 
             {session?.user.id === post.creator._id && pathName === "/profile" && (
-                <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
-                    <p className="font-inter text-sm green-gradient cursor-pointer"
-                        onClick={handleEdit}>
+                <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
+                    <p
+                        className='font-inter text-sm green_gradient cursor-pointer'
+                        onClick={handleEdit}
+                    >
                         Edit
                     </p>
-                    <p className="font-inter text-sm red-gradient cursor-pointer"
-                        onClick={handleDelete}>
+                    <p
+                        className='font-inter text-sm orange_gradient cursor-pointer'
+                        onClick={handleDelete}
+                    >
                         Delete
                     </p>
-                </div>)}
+                </div>
+            )}
         </div>
-    )
-}
+    );
+};
 
+<<<<<<< HEAD
 export default PromptCard
 >>>>>>> 1edbc47 (almost finished, search bar completed)
+=======
+export default PromptCard;
+>>>>>>> 589f005 (make cancel button in forms dynamic)
